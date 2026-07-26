8a3f07cb28cedd372d8595d69805b4bfae582f9a
CreamWasTaken <longswordx97@gmail.com>
2026-07-26 21:16:13 +0800

    Fixed Time bug on Order History and STock out

--- resources/js/lib/dates.js
+++ resources/js/lib/dates.js
@@ -13,7 +13,7 @@ export function toDate(value) {
     }
 
     const str = String(value).trim();
-    const dateOnlyMatch = str.match(/^(\d{4})-(\d{2})-(\d{2})/);
+    const dateOnlyMatch = str.match(/^(\d{4})-(\d{2})-(\d{2})$/);
 
     if (dateOnlyMatch) {
         const [, year, month, day] = dateOnlyMatch;
